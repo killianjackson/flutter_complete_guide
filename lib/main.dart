056bd347651d697a7989db2e6e97872656b855fe
killianjackson <killianjackson@yahoo.com>
2019-08-23 09:46:57 -0700

    Added Column widget and 4 children consisting of text and three raised button widgets. Section 2, Video 29

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,34 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text('Hello!'),);
+    var questions = [
+      'What\'s your favorite color?',
+      'What\'s your favorite animal?'
+    ];
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Column(
+          children: [
+            Text('The Question!'),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: null,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
