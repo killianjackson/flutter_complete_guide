ea81fbcaa5652c2803b23094e69e3ab4dc112e76
Killian Jackson <killian@ziprecruiter.com>
2019-10-06 19:29:45 -0700

    Finished Section 2: Flutter Basics

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/killian/killian-dev/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/killian/killian-dev/flutter_complete_guide"
+export "FLUTTER_TARGET=/Users/killian/killian-dev/flutter_complete_guide/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/killian/killian-dev/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
---
+++ lib/result.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class Result extends StatelessWidget {
+  final int resultScore;
+  final Function resetHandler;
+
+  Result(this.resultScore, this.resetHandler);
+
+  String get resultPhrase {
+    return "Score: ${resultScore.toString()}";
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: <Widget>[
+          Text(
+            resultPhrase,
+            style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center,
+          ),
+          FlatButton(
+            child: Text('Restart Quiz!'),
+            textColor: Colors.blue,
+            onPressed: resetHandler,
+          ),
+        ],
+      ),
+    );
+  }
+}
